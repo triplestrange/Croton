daad94e37ed8a8e775202888ff70d8f75c12b690
Robot-Nerd <djdierking@outlook.com>
2018-02-03 20:19:03 -0500

    Middle Switch Commentation

--- src/org/usfirst/frc/team1533/robot/commands/MiddleSwitch.java
+++ src/org/usfirst/frc/team1533/robot/commands/MiddleSwitch.java
@@ -8,32 +8,26 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MiddleSwitch extends CommandGroup {
 
 	public MiddleSwitch() {
-		// Add Commands here:
-		// e.g. addSequential(new Command1());
-		// addSequential(new Command2());
-		// these will run in order.
 		
 		//Sets Swerve wheels to forwards orientation
-		addSequential(new DriveDiagonalTime(0, 0, 0));
+		addSequential(new DriveDiagonalTime(0,0,0));
 		//Moves Swerve at an angle towards right Switch
-		addSequential(new DriveDiagonalTime(1.72, -.75, 65));
-		addSequential(new DriveDiagonalTime(3, 0, 0));
-		addSequential(new DriveDiagonalTime(.1, .1, 90));
-		addSequential(new DriveDiagonalTime(1.6, -.5, 0));
-		addSequential(new DriveDiagonalTime(1.9, -.75, 90));
-		//addSequential(new SwerveRotate(-1, .47));
-		//addSequential(new DriveForwardTime(1,.5));
-		//addSequential(new SwerveRotate(-1, .47));
-		// addSequential(new DriveDiagonalTime(.5, -.75, 0));
-
-		// To run multiple commands at the same time, use addParallel()
-		// e.g. addParallel(new Command1());
-		// addSequential(new Command2());
-		// Command1 and Command2 will run in parallel.
-
-		// A command group will require all of the subsystems that each member would
-		// require.
-		// e.g. if Command1 requires chassis, and Command2 requires arm, a CommandGroup
-		// containing them would require both the chassis and the arm.
+		addSequential(new DriveDiagonalTime(1.72,-.75,65));
+		//Acts as a 3 second pause while reorinetating the swerve wheels to 0 degrees
+		addSequential(new DriveDiagonalTime(3,0,0));
+		//Reverses Robot towards Driver Station
+		addSequential(new DriveDiagonalTime(.3,.1,90));
+		//"Slides" robot towards the Fence
+		addSequential(new DriveDiagonalTime(1.6,-.5,0));
+		//Moves Forward toward the Null Zone
+		addSequential(new DriveDiagonalTime(1.9,-.75,90));
+		//Rotates Swerve to face opposite fence
+		addSequential(new SwerveRotate(-1,.47));
+		//Moves Swerve towards mid-field
+		addSequential(new DriveDiagonalTime(1,-.5,0));
+		//Rotates Swerve to face a Power Cube
+		addSequential(new SwerveRotate(-1,.47));
+		//Moves Robot towards Power Cube
+		 addSequential(new DriveDiagonalTime(.5,-.75,0));
 	}
 }
