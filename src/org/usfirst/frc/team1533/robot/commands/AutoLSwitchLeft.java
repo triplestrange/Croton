ca25f6441e1e1368cd525c204ac1be5ffab77656
Robot-Nerd <djdierking@outlook.com>
2018-04-01 10:23:33 -0400

    Forsyth County Robot Code

--- src/org/usfirst/frc/team1533/robot/commands/AutoLSwitchLeft.java
+++ src/org/usfirst/frc/team1533/robot/commands/AutoLSwitchLeft.java
@@ -5,9 +5,10 @@ import org.usfirst.frc.team1533.robot.Constants;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoLSwitchLeft extends CommandGroup {
+	
 	public AutoLSwitchLeft() {
 		addParallel(new ElevatorProfile(10));
-		addSequential(new ArcProfile(130, -82.5, Constants.vCruise, 0, 0, 36, Constants.acc));
+		addSequential(new ArcProfile(130, -83, Constants.vCruise, 0, 0, 36, Constants.acc));
 		addParallel(new ElevatorProfile(15));
 		addParallel(new ArcProfile(48, -90, Constants.vCruise, 0, 36, 36, Constants.acc));
 		addSequential(new Intake(0.5, 1));
