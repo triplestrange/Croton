39d3dc58404ed49984c16ceb904cddf7291e7e42
1533 Alpha <32420432+team1533@users.noreply.github.com>
2018-07-12 14:26:07 -0400

    Untested mirrored code
    for 3 cube switch auto

--- src/org/usfirst/frc/team1533/robot/commands/PathAutoRSwitchRight.java
+++ src/org/usfirst/frc/team1533/robot/commands/PathAutoRSwitchRight.java
@@ -21,15 +21,33 @@ public class PathAutoRSwitchRight extends CommandGroup{
 				new SwerveWaypoint(-56, -12, Pathfinder.d2r(90), 0)
 				));
 		addSequential(new WaitCommand(1.5));
-		addSequential(new ElevatorProfile(0));
-		addSequential(new Intake(2,-1));
+		addParallel(new ElevatorProfile(0));
+		addSequential(new Intake(1.5,-1));
 		addParallel(new PathCommand(
 				new SwerveWaypoint(0, 0, Pathfinder.d2r(0), 0),
 				new SwerveWaypoint(18,-12, Pathfinder.d2r(15), 0),
 				new SwerveWaypoint(45, 60, Pathfinder.d2r(90), 0)
 				));
+		addSequential(new WaitCommand(1));
 		addSequential(new ElevatorProfile(15));
-		addSequential(new WaitCommand(2));
-		addSequential(new Intake(1,1));
+		addSequential(new WaitCommand(1));
+		addSequential(new Intake(0.5,1));
+		addParallel(new PathCommand(96,40,180,180,
+				new SwerveWaypoint(0, 0, Pathfinder.d2r(270), 0),
+				new SwerveWaypoint(0, -9, Pathfinder.d2r(240), -45),
+				new SwerveWaypoint(-48,-10, Pathfinder.d2r(0), -45)
+				));
+		addSequential(new WaitCommand(0.5));
+		addSequential(new ElevatorProfile(6));
+		addSequential(new Intake(1.25,-1));
+		addParallel(new PathCommand(96,40,90,180,
+				new SwerveWaypoint(0, 0, Pathfinder.d2r(180), -45),
+				new SwerveWaypoint(24, 0, Pathfinder.d2r(180), -45),
+				new SwerveWaypoint(48, 12, Pathfinder.d2r(180), 0)
+				));
+		addSequential(new WaitCommand(0.5));
+		addSequential(new ElevatorProfile(15));
+		addSequential(new WaitCommand(0.25));
+		addSequential(new Intake(0.5,1));
 }
 }
